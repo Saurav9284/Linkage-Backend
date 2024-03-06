6d5a9fa2b37ae327f97846b460760d7f5356f1cc
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-03-06 23:14:45 +0530

    Cloudinary added avatar are uploading successfully

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -2,12 +2,14 @@ const express = require('express')
 const UserModel = require('../Models/UserModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const upload = require('../Utils/multer')
+const cloudinary = require('../Utils/cloudinary')
 
 const UserController = express.Router()
 
 // Register/Signup
 
-UserController.post('/signup', async (req,res) => {
+UserController.post('/signup', upload.single('avatar'), async (req,res) => {
 
     const {name,phone,email,password} = req.body
 
@@ -19,25 +21,34 @@ UserController.post('/signup', async (req,res) => {
         if(exist){
             return res.send({msg:'User already exist wih this email, try another!'})
         }
-        bcrypt.hash(password,5, async function(err,hash){
-            if(err){
-                return res.send({msg:``})
+        cloudinary.uploader.upload(req.file.path, function(error, result) {
+            
+            if(error){
+                return res.send({msg:'Internal server error'})
             }
-            try {
-                const user = await UserModel.create({
-                    name:name,
-                    phone:phone,
-                    email:email,
-                    password:hash
-                })
-                console.log(user)
-                res.send({msg:'Signup successfull'})
-
-            } catch (error) {
-                console.log(error)
-                res.send({msg:'Internal servser error'})
-            }
-        })
+             const avatar = result.secure_url;
+             bcrypt.hash(password,5, async function(err,hash){
+                if(err){
+                    return res.send({msg:``})
+                }
+                try {
+                    const user = await UserModel.create({
+                        name:name,
+                        avatar:avatar,
+                        phone:phone,
+                        email:email,
+                        password:hash
+                    })
+                    console.log(user)
+                    res.send({msg:'Signup successfull'})
+    
+                } catch (error) {
+                    console.log(error)
+                    res.send({msg:'Internal servser error'})
+                }
+            })
+        });
+        
         
     } catch (error) {
         console.log(error)
---
+++ Utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+          
+cloudinary.config({ 
+  cloud_name: 'dezupfsqo', 
+  api_key: '115575235586454', 
+  api_secret: '2Bm9JhxIbaw3FCW_vPgfV4ZwoAQ' 
+});
+
+module.exports = cloudinary

34dfbb8053d879e91c7aea65de65d4890c159b13
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-03-07 12:50:35 +0530

    Login part done sending token and userdata in response

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const upload = require('../Utils/multer')
 const cloudinary = require('../Utils/cloudinary')
+const {JWT_SECRET} = require('../Config/db')
 
 
 const UserController = express.Router()
@@ -66,6 +67,47 @@ UserController.post('/signup', upload.single('avatar'), async (req,res) => {
     } catch (error) {
         console.log(error)
         res.send({msg:'Internal servser error'})
+    }
+});
+
+
+UserController.post('/login', async (req,res) => {
+
+    const { email , password } = req.body;
+
+    if(!email) {
+        return res.send({msg:'Email is required!'})
+    }
+    if(!password) {
+        return res.send({msg:'Password is required!'})
+    }
+
+    try {
+        const user = await UserModel.findOne({ email })
+        if(!user){
+            return res.send({msg:'Please signup first'})
+        }
+        bcrypt.compare(password,user.password, function(err,result){
+            if(result){
+                const token = jwt.sign({userId:user._id},JWT_SECRET)
+                return res.send({
+                    msg:'Login successfull',
+                    token:token,
+                    UserData:{
+                        name:user.name,
+                        email:user.email,
+                        avatar:user.avatar,
+                        phone:user.phone
+                    }
+                })
+            }
+            else{
+                res.send({msg:'Invalid credentials'})
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({msg:'Internal server error'})
     }
 });
 

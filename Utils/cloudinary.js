8f431aa0ff30217ca3aadfadbe774958e8f63363
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-03-21 13:50:51 +0530

    Update cloudinary.js

--- Utils/cloudinary.js
+++ Utils/cloudinary.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2
 require('dotenv').config()
-          
+
+
 cloudinary.config({ 
   cloud_name : process.env.cloud_name,
   api_key : process.env.api_key,
